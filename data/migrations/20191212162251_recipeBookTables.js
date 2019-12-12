2f3b2ebe3c9b7fefc4f2e7804898c38224b2f27c
Jess Daniel <jessdaniel474@gmail.com>
2019-12-12 17:06:30 -0500

    added knexfile with tables migration and empty seed files

---
+++ data/migrations/20191212162251_recipeBookTables.js
@@ -0,0 +1,62 @@
+exports.up = function(knex) {
+    return knex.schema
+        .createTable("recipes", tbl => {
+            tbl.increments();
+
+            tbl.string("name", 255)
+                .notNullable()
+                .unique();
+        })
+        .createTable("instructions", tbl => {
+            tbl.increments();
+
+            tbl.integer("recipe_id")
+                .unsigned()
+                .notNullable()
+                .references("id")
+                .inTable("recipes")
+                .onDelete("RESTRICT")
+                .onUpdate("CASCADE");
+            
+            tbl.integer("step_number")
+                .unsigned()
+                .notNullable();
+            
+            tbl.string("step_instructions", 255)
+                .notNullable();
+        })
+        .createTable("ingredients", tbl => {
+            tbl.increments();
+
+            tbl.string("name", 255)
+                .notNullable()
+                .unique();
+        })
+        .createTable("recipe_ingredients", tbl => {
+            tbl.primary(["recipe_id", "ingredient_id"]);
+
+            tbl.integer("recipe_id")
+                .unsigned()
+                .notNullable()
+                .references("id")
+                .inTable("recipes")
+                .onDelete("RESTRICT")
+                .onUpdate("CASCADE");
+            
+            tbl.integer("ingredient_id")
+                .unsigned()
+                .notNullable()
+                .references("id")
+                .inTable("ingredients")
+                .onDelete("RESTRICT")
+                .onUpdate("CASCADE");
+        })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists("recipe_ingredients")
+        .dropTableIfExists("ingredients")
+        .dropTableIfExists("instructions")
+        .dropTableIfExists("recipes");
+};
